038ed68ee5f47fdd1a4ef292fc46da46464b5e48
yun0209 <rhgt6002g@gmail.com>
2024-08-04 22:32:27 +0900

    푸터 반응형(부트스트랩) 진행중

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <footer id='yfooer' className='mb-5'>
 
             <div className='d-flex justify-content-between align-items-center mb-2'>
-                <p>어글리어스 고객센터 070.4769.0271</p>
+                <p className='d-none d-lg-block'>어글리어스 고객센터 070.4769.0271</p>
                 <ul className='d-none d-lg-block d-lg-flex justify-content-between me-0 ms-0 snsicon'>
                     <li>
                         <a href="#"><img src="/img/svg/sns_Instar.svg" alt="" />
@@ -31,8 +31,8 @@ function Footer() {
                 </ul>
             </div>
 
-            <div className='mb-5'>
-                <div className='d-flex align-items-center justify-content-between mb-5 tiem '>
+            <div className='d-none d-lg-block '>
+                <div className='d-lg-flex  align-items-center justify-content-between mb-5 tiem '>
                     <ul className=''>
                         <li>화-목요일</li>
                         <li>월·금요일</li>
@@ -56,26 +56,43 @@ function Footer() {
             </div>
 
             <div className='d-flex justify-content-between align-items-center'>
-                <h2>
+                <h2 className='d-none d-lg-block'>
                     <a href="#">
                        <img src="/img/svg/ugly_us_logo.svg" alt="로고" />
                     </a>
                 </h2>
-                <ul className='text-end about'>
-                    <li>
+                <ul className='text-lg-end  text-start about'>
+                    <li className='d-lg-flex'>
                         <p>
-                            <strong>상호명</strong>: 주식회사 캐비지 | <strong>대표</strong>: 최현주 | <strong>사업자등록번호</strong>: 572-81-02287 | <strong>소재지</strong>: 서울특별시 영등포구 양평로 144, 8층(양평동5가)
+                            <strong>상호명</strong>: 주식회사 캐비지
+                        </p>
+                        <p>
+                             <strong>대표</strong>: 최현주
+                        </p>
+                        <p>
+                           <strong>사업자등록번호</strong>: 572-81-02287
+
+                        </p>
+                        <p>
+                            <strong>소재지</strong>: 서울특별시 영등포구 양평로 144, 8층(양평동5가)
                         </p>
                     </li>
 
-                    <li>
+                    <li className='d-lg-flex'>
                         <p>
-                           <strong>통신판매업 신고번호 </strong>: 2021-화성동부-0302 | <strong>제휴 및 납품 문의</strong>: biz@uglyus.co.kr | <strong>농산물 납품 및 긴급구출 제보</strong>: farm@uglyus.co.kr
+                           <strong>통신판매업 신고번호 </strong>: 2021-화성동부-0302
+                        </p>
+                        <p>
+                           <strong>제휴 및 납품 문의</strong>: biz@uglyus.co.kr
+                        </p>
+                        <p>
+                           <strong>농산물 납품 및 긴급구출 제보</strong>: farm@uglyus.co.kr
                         </p>
                     </li>
 
-                    <li>
-                        서비스 이용약관개인정보 처리방침Copyright ⓒ 2024 주식회사 캐비지 All rights reserved.
+                    <li className='d-lg-flex '>
+                        <p>서비스 이용약관개인정보 처리방침 Copyright</p>
+                        <p>ⓒ 2024 주식회사 캐비지 All rights reserved.</p> 
                     </li>
                 </ul>
 
